4cc3d326d49d68b924ebeaab53356ab93911fc12
IsisGomezF <isiskgf@gmail.com>
2023-10-20 22:20:15 -0500

    Semaforo

--- main.js
+++ main.js
@@ -1,51 +1,20 @@
-/*const semaforo = true
-const avanza = "Verde";
-const pilas = "amarillo";
-const alto = "rojo";
-
-function avanzar (semaforo){
-    if(semaforo == true){
-        document.write ("<img src ='https://as1.ftcdn.net/v2/jpg/02/51/29/04/1000_F_251290437_bHYTygvYA70Kq3v1FurshY8KD3wivW4M.jpg'>");
-    }
-return avanzar ("verde")
-}
-
-function pilas (avanzar){
-    if(avanzar == "verde"){
-        document.write ("<img src='https://as1.ftcdn.net/v2/jpg/02/51/29/04/1000_F_251290422_RYHiF6h1vRljormdOguiqpJ3hW8afHdw.jpg'>");
-    }
-    return pilas ("amarillo")
-}
-
-https://as2.ftcdn.net/v2/jpg/02/51/29/03/1000_F_251290398_rHBsAbkXK9CdGSFPNCIK2TruvWlxE0uv.jpg
-
-avanzar ()
-document.write (avanzar)*/
-
-///-----------------------------------------
-/*const semaforo = ["verde","amarillo","rojo"]
-const cambio=true
+const semaforo = ["rojo","amarillo","verde"]
+const contenedor = document.getElementById("container");
+let index = 0;
 
 function colores (){
-    for (let index=0; index < semaforo.length; index++){
         if(semaforo[index]==="verde"){
-            document.write("<img src ='https://as1.ftcdn.net/v2/jpg/02/51/29/04/1000_F_251290437_bHYTygvYA70Kq3v1FurshY8KD3wivW4M.jpg'>");
+            contenedor.innerHTML="<img src ='https://as1.ftcdn.net/v2/jpg/02/51/29/04/1000_F_251290437_bHYTygvYA70Kq3v1FurshY8KD3wivW4M.jpg'>";
         }
         else if (semaforo[index]==="amarillo"){
-            document.write ("<img src='https://as1.ftcdn.net/v2/jpg/02/51/29/04/1000_F_251290422_RYHiF6h1vRljormdOguiqpJ3hW8afHdw.jpg'>");
+            contenedor.innerHTML="<img src='https://as1.ftcdn.net/v2/jpg/02/51/29/04/1000_F_251290422_RYHiF6h1vRljormdOguiqpJ3hW8afHdw.jpg'>";
         }
         else if (semaforo[index]==="rojo"){
-            document.write("<img src ='https://as2.ftcdn.net/v2/jpg/02/51/29/03/1000_F_251290398_rHBsAbkXK9CdGSFPNCIK2TruvWlxE0uv.jpg'>");
+            contenedor.innerHTML="<img src ='https://as2.ftcdn.net/v2/jpg/02/51/29/03/1000_F_251290398_rHBsAbkXK9CdGSFPNCIK2TruvWlxE0uv.jpg'>";
         }
+        index = (index + 1) % semaforo.length;
+        setTimeout(colores, 1000);
     }
- return setTimeout(colores, 2000);
-}
-colores()*/
 
-const semaforo = ["verde","amarillo","rojo"]
-const cambio=true
-
-function colores (){
-    
-}
+colores()
 
